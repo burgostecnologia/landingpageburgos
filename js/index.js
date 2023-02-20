2c27a21ccdd33019f053ec4c8b7c2ec44782f5e3
Roberto Junior <burgostecnologia@gmail.com>
2023-02-20 00:11:08 -0300

    repaginacao

---
+++ js/index.js
@@ -0,0 +1,18 @@
+// const cards = Array.from(document.querySelectorAll('.card'));
+// const middleX = cards[0].clientWidth / 2;
+// const middleY = cards[0].clientHeight / 2;
+
+// for(const card of cards){
+//     card.addEventListener('mousemove', (e) => {
+//         const frontCenterX = middleX - (e.clienteX - card.offsetLeft);
+//         const frontCenterY = middleY - (e.clienteY - card.offsetTop);
+//         card.style.setProperty('--rx', `${fromCenterY / 20}deg`);
+//         card.style.setProperty('--ry', `${fromCenterX / 20}deg`);        
+//     });
+
+//     card.addEventListener('mouseleave', (e) => {
+//         card.style.setProperty('--rx', 0);
+//         card.style.setProperty('--ry', 0);        
+//     });
+
+// }
